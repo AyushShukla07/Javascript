d01afa3614e7bd088aebf01234121042a3de3bc6
AyushShukla07 <154899126+AyushShukla07@users.noreply.github.com>
2024-11-14 11:57:05 +0000

    committed

---
+++ 10_API/01.js
@@ -0,0 +1,45 @@
+//API is nothing just a way of communication between frontend and backend
+//this frontend and backend may be of same user or frontend may be of one user and backend may be of any other user
+//frontend may be written in some other lamnguage and backend may be written in some other language
+//thus api is just a connenting medium between two ends
+
+
+//example of an api
+
+// {
+//     "login": "AyushShukla07",
+//     "id": 154899126,
+//     "node_id": "U_kgDOCTuStg",
+//     "avatar_url": "https://avatars.githubusercontent.com/u/154899126?v=4",
+//     "gravatar_id": "",
+//     "url": "https://api.github.com/users/AyushShukla07",
+//     "html_url": "https://github.com/AyushShukla07",
+//     "followers_url": "https://api.github.com/users/AyushShukla07/followers",
+//     "following_url": "https://api.github.com/users/AyushShukla07/following{/other_user}",
+//     "gists_url": "https://api.github.com/users/AyushShukla07/gists{/gist_id}",
+//     "starred_url": "https://api.github.com/users/AyushShukla07/starred{/owner}{/repo}",
+//     "subscriptions_url": "https://api.github.com/users/AyushShukla07/subscriptions",
+//     "organizations_url": "https://api.github.com/users/AyushShukla07/orgs",
+//     "repos_url": "https://api.github.com/users/AyushShukla07/repos",
+//     "events_url": "https://api.github.com/users/AyushShukla07/events{/privacy}",
+//     "received_events_url": "https://api.github.com/users/AyushShukla07/received_events",
+//     "type": "User",
+//     "user_view_type": "public",
+//     "site_admin": false,
+//     "name": null,
+//     "company": null,
+//     "blog": "",
+//     "location": null,
+//     "email": null,
+//     "hireable": null,
+//     "bio": null,
+//     "twitter_username": null,
+//     "public_repos": 1,
+//     "public_gists": 0,
+//     "followers": 1,
+//     "following": 0,
+//     "created_at": "2023-12-26T13:53:46Z",
+//     "updated_at": "2024-11-06T17:55:12Z"
+//   }
+
+//this api can be read using various formattors like jason formatter
